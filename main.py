ef559eda0caf50303c53861535393482a3ba1e94
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-08 19:02:14 +0200

    Add 'add' and 'edit' option

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def write_todos(local_todos, filepath='todos.txt'):
         file_local.writelines(local_todos)
 
 if not os.path.exists('todos.txt'):
-    with open('todos.txt', 'r') as file:
+    with open('todos.txt', 'w') as file:
         pass
 
 Sg.theme("Black")
@@ -21,12 +21,31 @@ add_button = Sg.Button("Add")
 edit_button = Sg.Button("Edit")
 close_button = Sg.Button("Close")
 complete_button = Sg.Button("Complete")
-list_box = Sg.Listbox(values="Hello", key='todos', size=(45, 10))
+list_box = Sg.Listbox(values=get_todos(), key='todos', size=(45, 10))
 
 layout = [[label], [[[input_box, add_button], [list_box, edit_button, complete_button]] , [close_button]]]
 
 window = Sg.Window('My To-Do App', layout=layout)
 
 while True:
-    window.read()
+    event, value = window.read()
+    match event:
+        case 'Add':
+            todos = get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            write_todos(todos)
+            window['todos'].update(todos)
+        case 'Edit':
+            todos = get_todos()
+            todo_to_edit = value['todos'][0]
+            new_todo = value['todo'] + '\n'
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            write_todos(todos)
+            window['todos'].update(todos)
+        case "Close":
+            break
+        case Sg.WIN_CLOSED:
+            break
 
