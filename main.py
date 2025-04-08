7df5392d77f3ea4dac4591de2fd8d1209cc26d90
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-08 18:54:34 +0200

    Add protection in case the .txt file doesn't exists

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import FreeSimpleGUI as Sg
+import os
 
 def get_todos(filepath = 'todos.txt'):
     with open(filepath, "r") as local_file:
@@ -8,6 +9,10 @@ def get_todos(filepath = 'todos.txt'):
 def write_todos(local_todos, filepath='todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(local_todos)
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'r') as file:
+        pass
 
 Sg.theme("Black")
 label = Sg.Text("Type in a To-Do: ")
