54f9f675907fadfb9b8c566c63b123069061b7d4
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-08 18:52:59 +0200

    Add functions for write and read todo on the file with extension .txt

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import FreeSimpleGUI as Sg
 
+def get_todos(filepath = 'todos.txt'):
+    with open(filepath, "r") as local_file:
+        todos_local = local_file.readlines()
+    return todos_local
+
+def write_todos(local_todos, filepath='todos.txt'):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(local_todos)
 
 Sg.theme("Black")
 label = Sg.Text("Type in a To-Do: ")
